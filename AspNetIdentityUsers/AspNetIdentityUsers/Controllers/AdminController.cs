5b4c665ab3e84c6a5c8da35be248ca4aebbfce57
alexey-savin <alexeyv.savin@gmail.com>
2017-03-17 18:00:05 +0300

    User creation logic implemented

---
+++ AspNetIdentityUsers/AspNetIdentityUsers/Controllers/AdminController.cs
@@ -0,0 +1,64 @@
+﻿using AspNetIdentityUsers.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
+using AspNetIdentityUsers.Models;
+using Microsoft.AspNet.Identity;
+
+namespace AspNetIdentityUsers.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(UserManager.Users);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+
+                IdentityResult result = 
+                    await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        private AppUserManager UserManager => HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+    }
+}
