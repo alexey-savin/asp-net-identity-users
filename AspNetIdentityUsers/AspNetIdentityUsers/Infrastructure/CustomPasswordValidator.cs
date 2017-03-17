1efcf80e9399ea7ba5f32b5a7bf090254089542b
alexey-savin <alexeyv.savin@gmail.com>
2017-03-17 18:21:19 +0300

    CustomPassword and User validation

---
+++ AspNetIdentityUsers/AspNetIdentityUsers/Infrastructure/CustomPasswordValidator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace AspNetIdentityUsers.Infrastructure
+{
+    public class CustomPasswordValidator : PasswordValidator
+    {
+        public override async Task<IdentityResult> ValidateAsync(string pass)
+        {
+            var result = await base.ValidateAsync(pass);
+
+            if (pass.Contains("12345"))
+            {
+                var errors = result.Errors.ToList();
+                errors.Add("Пароль не должен содержать последовательности чисел.");
+                result = new IdentityResult(errors);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AspNetIdentityUsers/AspNetIdentityUsers/Infrastructure/CustomUserValidator.cs
@@ -0,0 +1,30 @@
+﻿using AspNetIdentityUsers.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace AspNetIdentityUsers.Infrastructure
+{
+    public class CustomUserValidator : UserValidator<AppUser>
+    {
+        public CustomUserValidator(UserManager<AppUser, string> manager) : base(manager)
+        { }
+
+        public override async Task<IdentityResult> ValidateAsync(AppUser item)
+        {
+            var result = await base.ValidateAsync(item);
+
+            if (!item.Email.ToLower().EndsWith("@mymail.com"))
+            {
+                var errors = result.Errors.ToList();
+                errors.Add("Любой мыло-адрес, отличный от mymail.com запрещен");
+                result = new IdentityResult(errors);
+            }
+
+            return result;
+        }
+    }
+}
