882405bacb0a5cca53386060c3eb8e21eff3eaae
alexey-savin <alexeyv.savin@gmail.com>
2017-03-17 16:42:03 +0300

    Bootstrap package. Home Controller/View

---
+++ AspNetIdentityUsers/AspNetIdentityUsers/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace AspNetIdentityUsers.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("Key", "Value");
+
+            return View(data);
+        }
+    }
+}
