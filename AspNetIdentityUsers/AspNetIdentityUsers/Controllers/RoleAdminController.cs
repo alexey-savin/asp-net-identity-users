4544ea8d5d8595f8b6cc166050939ba93f68792f
alexey-savin <alexeyv.savin@gmail.com>
2017-04-13 16:29:52 +0300

    RoleAddminController

---
+++ AspNetIdentityUsers/AspNetIdentityUsers/Controllers/RoleAdminController.cs
@@ -0,0 +1,76 @@
+﻿using AspNetIdentityUsers.Infrastructure;
+using AspNetIdentityUsers.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetIdentityUsers.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        private AppUserManager UserManager => HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+
+        private AppRoleManager RoleManager => HttpContext.GetOwinContext().GetUserManager<AppRoleManager>();
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        public ActionResult Index() => View(RoleManager.Roles);
+
+        public ActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<ActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result =
+                    await RoleManager.CreateAsync(new AppRole(name));
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            AppRole role = await RoleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await RoleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View("Error", result.Errors);
+                }
+            }
+            else
+            {
+                return View("Error", new[] { "Role not found" });
+            }
+        }
+    }
+}
